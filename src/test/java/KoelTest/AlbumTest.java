b6b846f93329c72c3a6d1b09b50c54b1c717a24c
pavel <pavel.smoljaninov@yahoo.com>
2020-08-27 00:01:48 -0400

    updates

---
+++ src/test/java/KoelTest/AlbumTest.java
@@ -0,0 +1,91 @@
+package KoelTest;
+
+public class AlbumTest {
+
+    @Test
+    public void album_playlistTest_loginToKoel() {
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.openPage();
+        MainPage mainPage = loginPage.loginToKoel("testpro.user04@testpro.io","te$t$tudent");
+        Assert.assertTrue(mainPage.isMain());
+    }
+    @Test
+    public void album_playlistTest_createPlaylist() {
+        var loginPage = new LoginPage(driver);
+        loginPage.openPage();
+        var mainPage = loginPage.loginToKoel("testpro.user04@testpro.io","te$t$tudent");
+        String playlistId = mainPage.createNewPlaylist("xxxTestPLAYllllliiist");
+        Assert.assertTrue(mainPage.isPlaylistExist(playlistId));
+    }
+    @Test
+    public void album_playlistTest_playlist7003exist(){
+        var loginPage = new LoginPage(driver);
+        loginPage.openPage();
+        var mainPage = loginPage.loginToKoel("testpro.user04@testpro.io","te$t$tudent");
+        mainPage.isMain();
+        Assert.assertTrue(mainPage.isPlaylistExist("7003"));
+    }
+    @Test
+    public void album_playlistTest_wrongLogin(){
+        var loginPage = new LoginPage(driver);
+        loginPage.openPage();
+        loginPage.loginToKoel("testpro.user04@testpro.io","wrongPassword");
+        Assert.assertTrue(loginPage.isWrongLogin());
+    }
+    @Test
+    public void album_playlistTest_loginToKoel1() {
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.openPage();
+        MainPage mainPage = loginPage.loginToKoel("testpro.user04@testpro.io","te$t$tudent");
+        Assert.assertTrue(mainPage.isMain());
+    }
+    @Test
+    public void album_playlistTest_createPlaylist1() {
+        var loginPage = new LoginPage(driver);
+        loginPage.openPage();
+        var mainPage = loginPage.loginToKoel("testpro.user04@testpro.io","te$t$tudent");
+        String playlistId = mainPage.createNewPlaylist("xxxTestPLAYllllliiist");
+        Assert.assertTrue(mainPage.isPlaylistExist(playlistId));
+    }
+    @Test
+    public void album_playlistTest_playlist7003exist1(){
+        var loginPage = new LoginPage(driver);
+        loginPage.openPage();
+        var mainPage = loginPage.loginToKoel("testpro.user04@testpro.io","te$t$tudent");
+        mainPage.isMain();
+        Assert.assertTrue(mainPage.isPlaylistExist("7003"));
+    }
+    @Test
+    public void album_playlistTest_wrongLogin1(){
+        var loginPage = new LoginPage(driver);
+        loginPage.openPage();
+        loginPage.loginToKoel("testpro.user04@testpro.io","wrongPassword");
+        Assert.assertTrue(loginPage.isWrongLogin());
+    }
+    @Test
+    public void album_playlistTest_loginToKoel2() {
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.openPage();
+        MainPage mainPage = loginPage.loginToKoel("testpro.user04@testpro.io","te$t$tudent");
+        Assert.assertTrue(mainPage.isMain());
+    }
+    @Test
+    public void album_playlistTest_createPlaylist2() {
+        var loginPage = new LoginPage(driver);
+        loginPage.openPage();
+        var mainPage = loginPage.loginToKoel("testpro.user04@testpro.io","te$t$tudent");
+        String playlistId = mainPage.createNewPlaylist("xxxTestPLAYllllliiist");
+        Assert.assertTrue(mainPage.isPlaylistExist(playlistId));
+    }
+    @Test
+    public void album_playlistTest_wrongLogin2(){
+        var loginPage = new LoginPage(driver);
+        loginPage.openPage();
+        loginPage.loginToKoel("testpro.user04@testpro.io","wrongPassword");
+        Assert.assertTrue(loginPage.isWrongLogin());
+    }
+
+
+
+
+}
---
+++ src/test/java/KoelTest/BaseTest.java
@@ -0,0 +1,19 @@
+package KoelTest;
+
+public class BaseTest {
+
+    protected WebDriver driver;
+
+    @BeforeMethod
+    public void startUp() throws NoSuchFieldException {
+        driver = BrowserFabric.getDriver(BrowserType.CHROME);
+    }
+    @AfterMethod
+    public void tearDown(ITestResult iTestResult) throws InterruptedException {
+        if(iTestResult.getStatus()==iTestResult.FAILURE){
+            GetScreenshot.capture(driver,iTestResult.getName());
+        }
+        Thread.sleep(5000);
+        driver.quit();
+    }
+}
